37ac982dc605cd0e0d11ee35fdbc42ab6b2c66ad
Estudiante <estudiante@UCUXA40137.continental.edu.pe>
2024-05-03 18:41:08 -0500

    Maximo Comun Divisor - C#

---
+++ PROYECTOMCD/TDD_MCD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_MCD
+{
+    public class TDD_MCD
+    {
+        public static int MaximoComunDivisor(int nro1, int nro2)
+        {
+            while (nro2 != 0)
+            {
+                int temp = nro2;
+                nro2 = nro1 % nro2;
+                nro1 = temp;
+            }
+            return nro1;
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROYECTO_MCD;
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MaximoComunDivisorTestMethod1()
+        {
+            int nro1 = 10;
+            int nro2 = 4;
+            int esperado = 2;
+            int prueba = TDD_MCD.MaximoComunDivisor(nro1, nro2);
+            Assert.AreEqual(esperado, prueba);
+        }
+    }
+}
